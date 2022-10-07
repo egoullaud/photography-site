991cc613256a316369662ca3da07cb1c9bf2121f
elizabeth.goullaud <egoullaud@gmail.com>
2022-10-06 18:13:38 -0700

    style updated to black and white

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,6 @@
-const burger = document.querySelector("nav svg");
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(" nav .nav-links");
 
 burger.addEventListener("click", () => {
-  if (burger.classList.contains("active")) {
-    gsap.to(".dropdown", { x: "100%" });
-    gsap.to(".line", { stroke: "white" });
-    gsap.set("body", { overflow: "auto" });
-    gsap.set("body", { overflowX: "hidden" });
-  } else {
-    gsap.to(".dropdown", { x: "0%" });
-    gsap.to(".line", { stroke: "white" });
-
-    gsap.fromTo(
-      ".dropdown button",
-      { opacity: 0, y: 0 },
-      { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
-    );
-    gsap.set("body", { overflow: "hidden" });
-  }
-  burger.classList.toggle("active");
+  navLinks.classList.toggle("active");
 });
